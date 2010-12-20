ade3b8507d78f2f825201aa9a9131d1d0d89c290
mrbrdo <mrbrdo@zephyrus.mrbrdo.net>
2010-12-20 21:47:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jstecaj::Application.config.secret_token = '685fbd547f05fe0862570a4f5d2b83f91d28ee335ac0fed124779d24e21612af78ebfdcef80869c307da696f28e8619737f2cdcf1e62ec14c7ee6629f79218e1'
